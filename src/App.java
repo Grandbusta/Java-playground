6d8cea3aa04b2871ce5befe9e0ebbab1cc5689a5
Grandbusta <bustajay30@gmail.com>
2023-02-28 14:33:10 +0100

    initial commit

---
+++ src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        // System.out.println("Hello, World! What's your name?");
+        // Scanner scanner=new Scanner(System.in);
+        // String name =scanner.nextLine();
+        // System.out.println("Hello "+name);
+        String string =new String("Fucking string");
+        final int x=5;
+        String c="what";
+        System.out.println(c);
+        System.out.println(x);
+        float ff=20.5f;
+    }
+}
